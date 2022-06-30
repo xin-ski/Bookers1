452eb596d0bb7ad8225d5fc6dcda582b69ccceb7
EC2 Default User <ec2-user@ip-172-31-43-106.ec2.internal>
2022-06-30 13:10:14 +0000

    [add] validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
-    @book = Book.all
+    @books = Book.all
   end
 
   def show
@@ -13,17 +13,24 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated." #update　フラッシュメッセージ
-    redirect_to  "/books/#{book.id}"
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated." #update　フラッシュメッセージ
+      redirect_to  "/books/#{@book.id}"
+    else
+      render :edit
+    end
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created." #title, body追加時のフラッシュメッセージ
-    redirect_to "/books/#{book.id}"
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully created." #title, body追加時のフラッシュメッセージ
+      redirect_to "/books/#{@book.id}" #show.html
+    else
+      render :index
+    end
   end
 
   def destroy
